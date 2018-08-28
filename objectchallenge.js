0c4ea48f21eebd36220493bee5706602820502a7
mohammad <khezri@outlook.com>
2018-08-28 09:25:40 -0500

    in progress

---
+++ objectchallenge.js
@@ -0,0 +1,12 @@
+let elizabethSanger = {
+    congressionalDistrict: 5,
+        statements: [
+        {statememt: " Pie for everyone", category: "jobs"},
+        {statememt: " Pie for everyone", category: "taxes"},
+        {statememt: " Pie for everyone", category: "jobs"}
+    ],
+
+    donationFormUrl: 'www.google.com',
+    events:[
+        {date: '08/27/2018', title:'' }
+    ]
